7a03af189cc22f568497b5016e1c45287300887e
ConcoMB <cmaderblanco@gmail.com>
2013-10-15 17:50:01 -0300

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ss::Application.config.secret_token = 'c10bf6f58e265fd9d394444b657d8853aa181d073db4fc3404dad5e3cc0e345b97d7e12272c3002dc979718a315b8c3c55396fba73bf6ccac0d151e2169f368c'
